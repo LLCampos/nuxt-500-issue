d4913b36a41e2006bff6dd897b61c38115d014b6
LLCampos <luis.filipe.lcampos@gmail.com>
2022-05-13 16:52:34 +0100

    init

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div>
+        <h1>
+          {{ error.statusCode }}
+        </h1>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'Error',
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+})
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+    <button @click="bam">Trigger Error</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+  methods: {
+    bam() {
+      throw new Error('BAM!')
+    }
+  }
+})
+</script>
